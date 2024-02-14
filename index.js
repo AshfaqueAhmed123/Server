eb76556d106f25a01820bebee171e25accd74d25
Ashfaque Ahmed <155979001+AshfaqueAhmed123@users.noreply.github.com>
2024-02-14 13:14:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send(`
+    <h1>hello world</h1> 
+    `);
 });
 
 const port = process.env.PORT || 3000; // Use environment variable or default port 3000
