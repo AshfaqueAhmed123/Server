6db092528afdb3d74c4253a391ba30d758d18bcd
Ashfaque Ahmed <155979001+AshfaqueAhmed123@users.noreply.github.com>
2024-02-14 13:27:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send(`
-    <h1 style='text-align:center;color:blue;'>welcom to our server!</h1> 
+     <h1 style="text-align: center;color:blue;">welcome to our server</h1>
+    <h2 style="text-align: center; color: red;">this server si created using Express JS</h2>
+    
+    <p style="width: 80vw; margin: auto; text-align: center;">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Earum rerum facilis
+            assumenda esse
+            ducimus sit, dolor
+            quae harum ex laborum quos laboriosam necessitatibus totam voluptates? Quos molestias expedita, officia
+            architecto earum repellat tempore consectetur delectus doloremque! Aliquid sit ducimus explicabo itaque suscipit
+            dicta, nihil amet consequatur, eveniet saepe, vitae aliquam!</p>
     `);
 });
 
